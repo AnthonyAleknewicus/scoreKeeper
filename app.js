59c6c2996b5e68ee60c5c48ce4a6866c042c4afa
AnthonyAleknewicus <anthonyaleknewicus@gmail.com>
2024-01-19 20:39:51 -0800

    comments added 01

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Objects defining players one and two by initializing a zero score, and query selecting the id's of their individual buttons and displays from the HTML doc
 const p1 = {
     score: 0,
     button: document.querySelector('#p1Button'),
@@ -10,13 +11,15 @@ const p2 = {
     display: document.querySelector('#p2Display')
 }
 
-
+// Query selecting the 'Reset' button, and the 'Playing To' dropdown menu from the HTML doc and assigning each to a variable
 const resetBtn = document.querySelector('#reset');
 const winningScoreSelect = document.querySelector('#playTo');
 
+// setting variables to initialize a false 'Game Over' and that the game is defaulted to a winning score of '3' to align with the winningScoreSelect variable whose first option is also '3'
 let winningScore = 3;
 let isGameOver = false;
 
+// this function determines if the game is over by checking whether the player or opponents real time score is equal to the winningScore selected, after the player or opponents score is updated caused by the event listeners on their buttons when'clicked' to activate and call on this function.  If the game is over, winner's score will display green, and the loser will display red. The player and opponent buttons are also disabled until the game is 'reset'
 function updateScores(player, opponent) {
     if (!isGameOver) {
         player.score += 1;
@@ -32,6 +35,7 @@ function updateScores(player, opponent) {
 
 }
 
+// the event listeners which call upon updateScores when clicked
 p1.button.addEventListener('click', function () {
     updateScores(p1, p2);
 });
